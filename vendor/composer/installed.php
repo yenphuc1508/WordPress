469c2494da4f9f22d327dce6448411cd29491a9e
yenphuc1508 <22dh114399@st.huflit.edu.vn>
2025-07-28 02:12:57 +0700

    gg

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9fd2cdc6f6c7107c0a9cd1c527329095a9149d1d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9fd2cdc6f6c7107c0a9cd1c527329095a9149d1d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
