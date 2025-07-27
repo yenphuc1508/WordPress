9fd2cdc6f6c7107c0a9cd1c527329095a9149d1d
yenphuc1508 <22dh114399@st.huflit.edu.vn>
2025-07-28 01:37:32 +0700

    Second Commit

--- wp-config.php
+++ wp-config.php
@@ -21,14 +21,14 @@ define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
 
 // Security keys (generate tại https://api.wordpress.org/secret-key/1.1/salt/)
-define('AUTH_KEY',         'your-auth-key');
-define('SECURE_AUTH_KEY',  'your-secure-auth-key');
-define('LOGGED_IN_KEY',    'your-logged-in-key');
-define('NONCE_KEY',        'your-nonce-key');
-define('AUTH_SALT',        'your-auth-salt');
-define('SECURE_AUTH_SALT', 'your-secure-auth-salt');
-define('LOGGED_IN_SALT',   'your-logged-in-salt');
-define('NONCE_SALT',       'your-nonce-salt');
+define('AUTH_KEY',         '>!_^J*n8sYISA3/niGAc~p&7k5|52 Z1f/)_PTFT$[u.Q||Yx-?DOp/4(6g_nziP');
+define('SECURE_AUTH_KEY',  'Sgfd5XgL7U-52F.WD[Q5nf<9 {o)Zq-x^pw7J0aNET -v?sBkw[FTZK56xrPIGY|');
+define('LOGGED_IN_KEY',    'h}+-qvqga-2iqo#9nQx?yyuXxv9y<<f1yM. >%u<7|Q*G#9,ki6q$0M)h5Kfk*Nf');
+define('NONCE_KEY',        '}TVr-gca+([96aM=zk:u9C4uT/(t0D,Yx~N*t3IaBWx} +a2!e6}%fs%wS9h=QJ6');
+define('AUTH_SALT',        ';dKIS3Q<Yq{,fzuNayEg-.I|dQx$,Y9bP`C7Kk@A<O-~!q]Im-B2e2Wb5Sip2)rP');
+define('SECURE_AUTH_SALT', 'T|L -~!tDC1]lb:XZQZh&whnYPPyu4iPJ:P8Vdp=55=-2Y{u;6(-W[u`;|uDK8#F');
+define('LOGGED_IN_SALT',   'N#8?v#}k{,(J&,O^!+PtP9vAgtL& F/HMZ+tG-Mp`0a9fiA@I}e~.LPlj-@ckPte');
+define('NONCE_SALT',       ' @*&1v{y=8Vlm%DF<oR~73U|4l`svwx8p8u8TcbUsaA%-Q+cTywFOJV|xsXSg8D:');
 
 // Absolute path to WordPress directory
 if (!defined('ABSPATH')) {
